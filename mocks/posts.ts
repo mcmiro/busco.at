a5fef10756b36b3e91d624212ecb7aa3b38d8ac9
Miroljub Grujin <miroljub.grujin@bridgemaker.com>
2024-07-28 12:52:35 +0200

    fix: build error for the constants of posts

--- mocks/posts.ts
+++ mocks/posts.ts
@@ -9,13 +9,6 @@ const posts: PostType[] = [
       'https://images.unsplash.com/photo-1718965107564-daa12e777876?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tags: ['Klassenfahrten', 'Schulausflüge'],
     href: '/service/bus-mieten-fuer-schulklassen',
-    colors: {
-      background: '#047857',
-      title: '#ECFDF5',
-      copy: '#ECFDF5',
-      textTag: '#064E3B',
-      backgroundTag: '#D8E5DE',
-    },
   },
   {
     title: 'Bus mieten für Shuttle-Service',
@@ -25,13 +18,6 @@ const posts: PostType[] = [
       'https://images.unsplash.com/photo-1719176372654-352eaa75f065?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tags: ['Für jeden Anlass', 'Ideal für Events'],
     href: '/service/bus-mieten-fuer-shuttle-service',
-    colors: {
-      background: '#E5E7EB',
-      title: '#4338CA',
-      copy: '#1F2937',
-      textTag: '#4338CA',
-      backgroundTag: '#C7D2FE',
-    },
   },
   {
     title: 'Bus mieten für Betriebsfahrten',
@@ -41,13 +27,6 @@ const posts: PostType[] = [
       'https://images.unsplash.com/photo-1718486463454-a29ee090f0c5?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tags: ['Firmenausflüge', 'Team-Building Reisen'],
     href: '/service/bus-mieten-fuer-betriebsfahrten',
-    colors: {
-      background: '#4338CA',
-      title: '#E5E7EB',
-      copy: '#E5E7EB',
-      textTag: '#1F2937',
-      backgroundTag: '#E5E7EB',
-    },
   },
   {
     title: 'Bus mieten für Betriebsfahrten',
@@ -57,13 +36,6 @@ const posts: PostType[] = [
       'https://images.unsplash.com/photo-1718486463454-a29ee090f0c5?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tags: ['Firmenausflüge', 'Team-Building Reisen'],
     href: '/service/bus-mieten-fuer-betriebsfahrten',
-    colors: {
-      background: '#0D9488',
-      title: '#ECFDF5',
-      copy: '#ECFDF5',
-      textTag: '#134E4A',
-      backgroundTag: '#D6FAF1',
-    },
   },
 ];
 
